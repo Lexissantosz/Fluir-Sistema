0d1880d71667425d5cf5320e5df086bed775c0e9
alex <Lexissantoosz@gmail.com>
2026-08-28 11:02:04 -0300

    fix: libera frontend da Vercel no CORS

--- backend/src/main/java/com/fluir/backend/config/CorsConfig.java
+++ backend/src/main/java/com/fluir/backend/config/CorsConfig.java
@@ -19,6 +19,7 @@ public class CorsConfig {
                                 "http://localhost:5500",
                                 "http://localhost:5173",
                                 "https://lexissantosz.github.io"
+                                "https://fluir-sistema.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
