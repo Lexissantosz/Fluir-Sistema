2cf99b19db65a685e646d7e0c62e0a33355427e0
Alex Santos Inacio Augusto <Lexissantoosz@gmail.com>
2026-09-25 17:12:36 -0300

    refactor: amplia limite de registro de agua para 10l

--- js/water.js
+++ js/water.js
@@ -81,7 +81,7 @@ const waterRegistrationMessage = document.getElementById("waterRegistrationMessa
 // =====================================================
 
 const MIN_WATER_PER_REGISTRATION_ML = 1;
-const MAX_WATER_PER_REGISTRATION_ML = 2000;
+const MAX_WATER_PER_REGISTRATION_ML = 10000;
 
 const defaultSetup = {
   user: {
